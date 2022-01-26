55f331ee40016d71ce0543d64684b54c67fab9e4
Tomasz Pieścikowski <63928070+tomaszpiescikowski@users.noreply.github.com>
2022-01-26 19:20:48 +0100

    Update oracle_connection.py

--- important/oracle_connection.py
+++ important/oracle_connection.py
@@ -1,8 +1,8 @@
 import cx_Oracle
 
 try:
-    cx_Oracle.init_oracle_client(lib_dir=r"C:\Users\tomek\Desktop\SQL\instantclient_21_3")
-    conn = cx_Oracle.connect('inf145418/inf145418@//admlab2.cs.put.poznan.pl:1521/dblab02_students.cs.put.poznan.pl')
+    cx_Oracle.init_oracle_client(lib_dir=r"<Sciezka instant client>")
+    conn = cx_Oracle.connect('<twojlogin>/<twojehaslo>@//admlab2.cs.put.poznan.pl:1521/dblab02_students.cs.put.poznan.pl')
 except Exception as err:
     print('Blad podczas proby nawiazania polaczenia: ', err)
 else:
